3981cc20b1140f961179db56651206148ad3a465
Ayesha <ayesha19765@gmail.com>
2024-07-03 02:55:03 +0530

    added source files

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://8d0a341ab4bfb5837b90054da12f8847@o4507534223409152.ingest.us.sentry.io/4507534252638208",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
