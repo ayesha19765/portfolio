e576088f5ace74137fe3f53b7a174e4f0b5904e1
Ayesha <ayesha19765@gmail.com>
2024-12-06 23:30:38 +0530

    Updated data

---
+++ app/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { useEffect, useState } from "react";
+import { navItems } from "@/data";
+import { Loader } from "@/components/Loader1";
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Clients from "@/components/Clients";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+const Home = () => {
+	// State to track loading
+	const [isLoading, setIsLoading] = useState(true);
+
+	// Scroll to Hero on component mount
+	useEffect(() => {
+		const heroSection = document.querySelector("#hero");
+		if (heroSection) {
+			heroSection.scrollIntoView({ behavior: "smooth" });
+		}
+
+		// Simulate loading process (can be replaced with actual data fetching logic)
+		const timer = setTimeout(() => {
+			setIsLoading(false); // Set loading to false after 2 seconds
+		}, 1400);
+
+		// Clean up timer on unmount
+		return () => clearTimeout(timer);
+	}, []);
+
+	return (
+		<div className="min-h-screen flex justify-center items-center flex-col bg-black-100 overflow-hidden mx-auto sm:px-10 px-5 ">
+			{/* Loader component */}
+			<Loader
+				isLoading={isLoading}
+				onTransitionEnd={() => console.log("Transition ended")}
+			/>
+
+			{/* Only render the content after the loader is hidden */}
+			{!isLoading && (
+				<>
+					<header>
+						<FloatingNav navItems={navItems} />
+					</header>
+					<main className="max-w-7xl w-full">
+						<section id="hero">
+							<Hero />
+						</section>
+						<section>
+							<Grid />
+						</section>
+						<section>
+							<RecentProjects />
+						</section>
+						<section>
+							<Clients />
+						</section>
+					</main>
+					<footer id="footer" className="py-10">
+						<Footer />
+					</footer>
+				</>
+			)}
+		</div>
+	);
+};
+
+export default Home;
---
+++ data/index.ts
@@ -0,0 +1,360 @@
+export const navItems = [
+	{ name: "Home", link: "#home" },
+	{ name: "About", link: "#about" },
+	{ name: "Projects", link: "#projects" },
+	{ name: "Skills", link: "#skills" },
+	{ name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+	// {
+	//   id: 1,
+	//   title: "I prioritize client collaboration, fostering open communication ",
+	//   description: "",
+	//   className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+	//   imgClassName: "w-full h-full",
+	//   titleClassName: "justify-end",
+	//   img: "/b1.svg",
+	//   spareImg: "",
+	// },
+	// {
+	//   id: 2,
+	//   title: "I'm very flexible with time zone communications",
+	//   description: "",
+	//   className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+	//   imgClassName: "",
+	//   titleClassName: "justify-start",
+	//   img: "",
+	//   spareImg: "",
+	// },
+	// {
+	//   id: 3,
+	//   title: "My tech stack",
+	//   description: "I constantly try to improve",
+	//   className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+	//   imgClassName: "",
+	//   titleClassName: "justify-center",
+	//   img: "",
+	//   spareImg: "",
+	// },
+	// {
+	//   id: 4,
+	//   title: "Tech enthusiast with a passion for development.",
+	//   description: "",
+	//   className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+	//   imgClassName: "",
+	//   titleClassName: "justify-start",
+	//   img: "/grid.svg",
+	//   spareImg: "/b4.svg",
+	// },
+
+	// {
+	//   id: 5,
+	//   title: "Currently building a JS Animation library",
+	//   description: "The Inside Scoop",
+	//   className: "md:col-span-3 md:row-span-2",
+	//   imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+	//   titleClassName: "justify-center md:justify-start lg:justify-center",
+	//   img: "/b5.svg",
+	//   spareImg: "/grid.svg",
+	// },
+	// {
+	//   id: 6,
+	//   title: "Do you want to start a project together?",
+	//   description: "",
+	//   className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+	//   imgClassName: "",
+	//   titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+	//   img: "",
+	//   spareImg: "",
+	// },
+	{
+		id: 1,
+		title:
+			" I love tackling challenges and continuously evolving in the dynamic tech space",
+		description: "",
+		className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+		imgClassName: "w-full h-full opacity-70",
+		titleClassName: "justify-end",
+		img: "/b3.jpg",
+		spareImg: "",
+	},
+	{
+		id: 2,
+		title: "Next.js: My Playground for Web Innovation",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 3,
+		title: "My tech stack",
+		description: "I constantly try to improve",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-center",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 4,
+		title: "Tech enthusiast with a passion for development.",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "/grid.svg",
+		spareImg: "/b4.svg",
+	},
+
+	{
+		id: 5,
+		title: "Exploring the Core of Problem-Solving: DSA in Focus",
+		description: "The Inside Scoop",
+		className: "md:col-span-3 md:row-span-2",
+		imgClassName: "absolute right-0 bottom-0 md:w-[35rem] w-[20rem] opacity-65",
+		titleClassName: "justify-center md:justify-start lg:justify-center",
+		img: "/b5.png",
+		spareImg: "/grid.svg",
+	},
+	{
+		id: 6,
+		title: "Do you want to start a project together?",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+		img: "",
+		spareImg: "",
+	},
+];
+
+export const projects = [
+	{
+		id: 1,
+		title: "Ash Foods to Order Food",
+		des: "Experience the future of dining with our seamless and intuitive food ordering platform built using the MERN stack. Enjoy a world of culinary delights at your fingertips",
+		img: "/AshFoods.png",
+		// iconLists: ['/re.svg', '/tail.svg', '/ts.svg', '/three.svg', '/fm.svg'],
+		iconLists: [
+			"/mongo.svg",
+			"/node.svg",
+			"/express.svg",
+			"/react.svg",
+			"/icons/Bootstrap.svg",
+		],
+		link: "https://ash-foods.vercel.app/",
+	},
+	// {
+	// 	id: 1,
+	// 	title: "3D Solar System Planets to Explore",
+	// 	des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+	// 	img: "/p1.svg",
+	// 	iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+	// 	link: "/ui.earth.com",
+	// },
+	{
+		id: 2,
+		title: "CozyStay for hotel booking",
+		des: "Discover the elegance of our hotel building with this immersive 3D virtual tour built using the powerful MERN stack",
+		img: "/cozyStay.png",
+		iconLists: ["/mongo.svg", "/node.svg", "/express.svg", "/react.svg"],
+		link: "https://github.com/ayesha19765/CozyStay",
+	},
+	{
+		id: 3,
+		title: "Nayasa Chatbot - using NLP",
+		des: "A cutting-edge website with AI chatbot technology for seamless food ordering, featuring natural conversations, personalized recommendations, and efficient order management using the latest tech stack",
+		img: "/chatbot.jpeg",
+		iconLists: [
+			"/icons/DialogFlow.svg",
+			"/icons/ngrok.svg",
+			"/icons/FastApi.svg",
+		],
+		link: "https://github.com/ayesha19765/Smart-Chatbot-Integration-for-Enhanced-Food-Ordering-Experience",
+	},
+	{
+		id: 4,
+		title: "Innovative Portfolio with Next.js, Three.js, and Tailwind CSS",
+		des: "Discover my cutting-edge web portfolio, crafted with Next.js, Three.js, and Tailwind CSS. Experience seamless navigation, dynamic 3D visualizations, and responsive design, showcasing my expertise in modern web development.",
+		img: "/portfolio.png",
+		iconLists: ["/next.svg", "/tail.svg", "/three.svg", "/icons/Node.js.svg"],
+		link: "https://github.com/ayesha19765/portfolio",
+	},
+];
+
+// export const testimonials = [
+// 	{
+// 		quote:
+// 			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+// 		name: "Michael Johnson",
+// 		title: "Director of AlphaStream Technologies",
+// 	},
+// 	{
+// 		quote:
+// 			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+// 		name: "Michael Johnson",
+// 		title: "Director of AlphaStream Technologies",
+// 	},
+// 	{
+// 		quote:
+// 			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+// 		name: "Michael Johnson",
+// 		title: "Director of AlphaStream Technologies",
+// 	},
+// 	{
+// 		quote:
+// 			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+// 		name: "Michael Johnson",
+// 		title: "Director of AlphaStream Technologies",
+// 	},
+// 	{
+// 		quote:
+// 			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+// 		name: "Michael Johnson",
+// 		title: "Director of AlphaStream Technologies",
+// 	},
+// ];
+
+// export const companies = [
+// 	{
+// 		id: 1,
+// 		name: "cloudinary",
+// 		img: "/cloud.svg",
+// 		nameImg: "/cloudName.svg",
+// 	},
+// 	{
+// 		id: 2,
+// 		name: "appwrite",
+// 		img: "/app.svg",
+// 		nameImg: "/appName.svg",
+// 	},
+// 	{
+// 		id: 3,
+// 		name: "HOSTINGER",
+// 		img: "/host.svg",
+// 		nameImg: "/hostName.svg",
+// 	},
+// 	{
+// 		id: 4,
+// 		name: "stream",
+// 		img: "/s.svg",
+// 		nameImg: "/streamName.svg",
+// 	},
+// 	{
+// 		id: 5,
+// 		name: "docker.",
+// 		img: "/dock.svg",
+// 		nameImg: "/dockerName.svg",
+// 	},
+// ];
+
+export const workExperience = [
+	{
+		id: 1,
+		title: "Full Stack Web Developer Intern",
+		company: "YahWeh Innovations",
+		timeline: "July 2024 - Present",
+		desc: "Boosted user engagement, conversions, and organic traffic through responsive web components, SEO optimization, and enhanced mobile usability during a Full Stack Web Development Internship.",
+		className: "md:col-span-2",
+		thumbnail: "/exp1.svg",
+	},
+	{
+		id: 1,
+		title: "Technical Head",
+		company: "Abhi",
+		timeline: "July 2024 - Present",
+		desc: "Boosted user engagement, conversions, and organic traffic through responsive web components, SEO optimization, and enhanced mobile usability during a Full Stack Web Development Internship.",
+		className: "md:col-span-2",
+		thumbnail: "/exp1.svg",
+	},
+	// {
+	// 	id: 1,
+	// 	title: "Frontend Engineer Intern",
+	// 	desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+	// 	className: "md:col-span-2",
+	// 	thumbnail: "/exp1.svg",
+	// },
+	// {
+	// 	id: 2,
+	// 	title: "Mobile App Dev - JSM Tech",
+	//    company: "YahWeh Innovations",
+	// 	desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+	// 	className: "md:col-span-2", // change to md:col-span-2
+	// 	thumbnail: "/exp2.svg",
+	// },
+	// {
+	// 	id: 3,
+	// 	title: "Freelance App Dev Project",
+	// 	desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+	// 	className: "md:col-span-2", // change to md:col-span-2
+	// 	thumbnail: "/exp3.svg",
+	// },
+	// {
+	// 	id: 4,
+	// 	title: "Lead Frontend Developer",
+	// 	desc: "Developed and maintained user-facing features using modern frontend technologies.",
+	// 	className: "md:col-span-2",
+	// 	thumbnail: "/exp4.svg",
+	// },
+];
+
+export const socialMedia = [
+	{
+		id: 1,
+		img: "/git.svg",
+		link: "https://github.com/ayesha19765",
+	},
+	{
+		id: 2,
+		img: "/leetcode.svg",
+		link: "https://leetcode.com/u/ayesha19765/",
+	},
+	{
+		id: 3,
+		img: "/link.svg",
+		link: "https://www.linkedin.com/in/ayesha19765/",
+	},
+];
+export const techStack = [
+	{ svg: "/icons/Express.svg" },
+	{ svg: "/icons/Node.js.svg" },
+	{ svg: "/icons/Tailwind CSS.svg" },
+	{ svg: "/icons/MongoDB.svg" },
+	{ svg: "/icons/Nodemon.svg" },
+	{ svg: "/icons/Three.js.svg" },
+	{ svg: "/icons/PostgresSQL.svg" },
+	{ svg: "/icons/CSS3.svg" },
+	{ svg: "/icons/JavaScript.svg " },
+	{ svg: "/icons/Next.js.svg" },
+	{ svg: "/icons/React.svg" },
+	{ svg: "/icons/HTML5.svg" },
+];
+export const languagesAndTools = [
+	{ svg: "/icons/Material UI.svg" },
+	{ svg: "/icons/Bootstrap.svg" },
+	{ svg: "/icons/Git.svg" },
+	{ svg: "/icons/Mongoose.js.svg" },
+	{ svg: "/icons/jQuery.svg" },
+	{ svg: "/icons/Markdown.svg" },
+	{ svg: "/icons/NPM.svg" },
+	{ svg: "/icons/MySQL.svg" },
+];
+export const frameWorks = [
+	{ svg: "/icons/C.svg" },
+	{ svg: "/icons/C++.svg" },
+	{ svg: "/icons/Java.svg" },
+	{ svg: "/icons/Python.svg" },
+	{ svg: "/icons/Xcode.svg" },
+	{ svg: "/icons/VS code.svg" },
+	{ svg: "/icons/Postman.svg" },
+	{ svg: "/icons/Canva.svg" },
+	{ svg: "/icons/Adobe Photoshop.svg" },
+	{ svg: "/icons/ngrok.svg" },
+	{ svg: "/icons/DialogFlow.svg" },
+	{ svg: "/icons/FastAPI.svg" },
+	// github
+];
