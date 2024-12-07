077ffd601f5bbcea19c42ad3dcee5f04ac546a5b
Ayesha <ayesha19765@gmail.com>
2024-12-07 12:45:35 +0530

    recent projects section

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -18,64 +18,123 @@ const RecentProjects = () => {
 						className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 md:min-w-[400px] "
 						key={item.id}
 					>
-						<PinContainer
-							title="Visit"
-							href={item.link}
-							className="w-[80vw] md:w-[30vw]"
+						{/* Large screens */}
+			<div className="hidden md:block">
+				<PinContainer
+					title="Visit"
+					href={item.link}
+					className="w-[80vw] md:w-[30vw]"
+				>
+					<a href={item.link}>
+						<div className="relative flex items-center justify-center w-[75vw] md:w-[28vw] overflow-hidden h-[20vh] lg:h-[30vh] m-4">
+							<div
+								className="relative w-full h-full overflow-hidden"
+								style={{ backgroundColor: "#13162D" }}
+							>
+								<img src="/bg.png" alt="bgimg" />
+							</div>
+							<img
+								src={item.img}
+								alt="cover"
+								className="z-10 absolute bottom-0 rotate-6"
+							/>
+						</div>
+
+						<h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+							{item.title}
+						</h1>
+
+						<p
+							className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+							style={{
+								color: "#BEC1DD",
+								margin: "1vh 0",
+							}}
 						>
-							<a href={item.link}>
-								<div className="relative flex items-center justify-center w-[75vw] md:w-[28vw] overflow-hidden h-[20vh] lg:h-[30vh] m-4">
+							{item.des}
+						</p>
+
+						<div className="flex items-center justify-between mt-7 mb-3">
+							<div className="flex items-center">
+								{item.iconLists.map((icon: string, index: number) => (
 									<div
-										className="relative w-full h-full overflow-hidden  "
-										style={{ backgroundColor: "#13162D" }}
+										key={index}
+										className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+										style={{
+											transform: `translateX(-${5 * index + 2}px)`,
+										}}
 									>
-										<img src="/bg.png" alt="bgimg" />
+										<img src={icon} alt="icon" className="p-2" />
 									</div>
-									<img
-										src={item.img}
-										alt="cover"
-										className="z-10 absolute bottom-0 rotate-6"
-									/>
-								</div>
+								))}
+							</div>
 
-								<h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-									{item.title}
-								</h1>
+							<div className="flex justify-center items-center">
+								<p className="flex lg:text-xl md:text-xs text-sm text-purple">
+									Check Live Site
+								</p>
+								<FaLocationArrow className="ms-3" color="#CBACF9" />
+							</div>
+						</div>
+					</a>
+				</PinContainer>
+			</div>
 
-								<p
-									className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+			{/* Small screens */}
+			<div className="block md:hidden">
+				<a href={item.link}>
+					<div className="relative flex items-center justify-center w-[75vw] overflow-hidden h-[20vh] lg:h-[30vh] m-4">
+						<div
+							className="relative w-full h-full overflow-hidden"
+							style={{ backgroundColor: "#13162D" }}
+						>
+							<img src="/bg.png" alt="bgimg" />
+						</div>
+						<img
+							src={item.img}
+							alt="cover"
+							className="z-10 absolute bottom-0 rotate-6"
+						/>
+					</div>
+
+					<h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+						{item.title}
+					</h1>
+
+					<p
+						className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+						style={{
+							color: "#BEC1DD",
+							margin: "1vh 0",
+						}}
+					>
+						{item.des}
+					</p>
+
+					<div className="flex items-center justify-between mt-7 mb-3">
+						<div className="flex items-center">
+							{item.iconLists.map((icon: string, index: number) => (
+								<div
+									key={index}
+									className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
 									style={{
-										color: "#BEC1DD",
-										margin: "1vh 0",
+										transform: `translateX(-${5 * index + 2}px)`,
 									}}
 								>
-									{item.des}
-								</p>
-
-								<div className="flex items-center justify-between mt-7 mb-3">
-									<div className="flex items-center">
-										{item.iconLists.map((icon, index) => (
-											<div
-												key={index}
-												className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-												style={{
-													transform: `translateX(-${5 * index + 2}px)`,
-												}}
-											>
-												<img src={icon} alt="icon5" className="p-2" />
-											</div>
-										))}
-									</div>
-
-									<div className="flex justify-center items-center">
-										<p className="flex lg:text-xl md:text-xs text-sm text-purple">
-											Check Live Site
-										</p>
-										<FaLocationArrow className="ms-3" color="#CBACF9" />
-									</div>
+									<img src={icon} alt="icon" className="p-2" />
 								</div>
-							</a>
-						</PinContainer>
+							))}
+						</div>
+
+						<div className="flex justify-center items-center">
+							<p className="flex lg:text-xl md:text-xs text-sm text-purple">
+								Check Live Site
+							</p>
+							<FaLocationArrow className="ms-3" color="#CBACF9" />
+						</div>
+					</div>
+				</a>
+			</div>
 					</div>
 				))}
 			</div>
